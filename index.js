c01652f6df58addce250c43b86b8902c8a0d6434
pazzo-rozza <m.rrozhani.3283@vallauri.edu>
2025-04-30 11:45:24 +0200

    commit
    implementazione tabella e inizio messa dei 2 in posizioni causali

--- index.js
+++ index.js
@@ -2,10 +2,32 @@
 
 const DIM = 4;
 
-window.onload=function(){
+window.onload = function () {
+   const tabella = document.getElementById("wrapper");
+   for (let i = 0; i < DIM; i++) {
 
+      for (let j = 0; j < DIM; j++) {
+         const cella = document.createElement("div");
+         cella.id = `div-${i}-${j}`;
+         cella.classList = "cella"
+
+         tabella.appendChild(cella);
+      }
+   }
+   let a, b, c, d;
+   do {
+      a = generaNumero(0, 4);
+      b = generaNumero(0, 4);
+      c = generaNumero(0, 4);
+      d = generaNumero(0, 4);
+      alert(a);
+      alert(b);
+      alert(c);
+      alert(d);
+   } while (a == c && b == d);
+   let 
 }
 
-function generaNumero(min, max){
-   return Math.floor((max - min) * Math.random()) + min;   
+function generaNumero(min, max) {
+   Math.floor(Math.random() * (max - min)) + min;
 }
